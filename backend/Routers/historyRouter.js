597d652dfeceac4d1199d78d00f5fc979d43a7de
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-09-24 01:53:15 +0530

    Fixed Database Store Translation Details without Authenticated. Broken Authentication Vulnerability

--- backend/Routers/historyRouter.js
+++ backend/Routers/historyRouter.js
@@ -1,13 +1,16 @@
 import express from "express";
 import historyController from "../controllers/historyController.js";
+import userController from "../controllers/userController.js";
 
 const historyRouter = express.Router();
 
-historyRouter.post("/save", historyController.createHistory);
+historyRouter.post(
+  "/save",
+  userController.verify,
+  historyController.createHistory
+);
 historyRouter.get("/getHistory", historyController.getHistory);
 historyRouter.delete("/deleteHistory/:id", historyController.deleteHistory);
 historyRouter.delete("/clearAllData", historyController.clearAllData);
-
-
 
 export default historyRouter;
